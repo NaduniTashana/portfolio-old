30c057cff031d4707b5af12fef6edc2be5a5c211
NaduniTashana <nadutash0@gmail.com>
2024-05-29 00:44:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById('downloadButton').addEventListener('click', function() {
+    var downloadLink = document.getElementById('downloadLink');
+    downloadLink.setAttribute('href', 'https://drive.google.com/file/d/1WBBIpjcnPFb6M-SyHgtsgHydekmjgXe5/view?usp=sharing'); 
+    downloadLink.click();
+});
+
+
